99f07ff2e2025e60b5b925a553bfc766abcd4b69
CelineAsadi <celineassadi44@gmail.com>
2025-09-17 17:15:52 +0300

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 // import React from 'react';
-//hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-//heloooooooooooooooooooooo
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-//mmmmmmmmmmm
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
