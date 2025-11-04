fe49fd42432086c9daec3357111b071659416eb6
CelineAsadi <celineassadi44@gmail.com>
2025-11-04 14:55:55 +0200

    c

--- server/src/lib/utlis.js
+++ server/src/lib/utlis.js
@@ -5,21 +5,14 @@ const generateToken = (userId, res)=>{
         expiresIn:'7d'
     });
 
-  res.cookie('pedaTrain', token, {
-  httpOnly: true,
-  secure: true,        // חובה בפרודקשן + HTTPS
-  sameSite: 'None',    // חובה כששולחים לדומיין אחר
-  maxAge: 7 * 24 * 60 * 60 * 1000
-});
+    res.cookie('pedaTrain',token,{
+        maxAge: 7 * 24 * 60 * 1000, // 7 days in milliseconed
+        httpOnly:true, // prevent XSS attacks
+        sameSite:'strict', // prevent CSRF attacks
+        secure: process.env.NODE_ENV !== 'development' // use https in production
+    });
     return token;
 };
 
 module.exports = generateToken;
 
-
-    // res.cookie('pedaTrain',token,{
-    //     maxAge: 7 * 24 * 60 * 1000, // 7 days in milliseconed
-    //     httpOnly:true, // prevent XSS attacks
-    //     sameSite:'strict', // prevent CSRF attacks
-    //     secure: process.env.NODE_ENV !== 'development' // use https in production
-    // });
