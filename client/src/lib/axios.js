67205a7b4f82ad9840e95d2d48423d6bbaf7d0c9
CelineAsadi <celineassadi44@gmail.com>
2025-11-04 10:11:36 +0200

    c

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -10,9 +10,10 @@ import axios from "axios";
 // שם לב: שמנו /api ב-baseURL כדי שהקריאות שלך כמו '/auth/Login'
 // יהפכו בפועל ל: http://localhost:4000/api/auth/Login
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://pedagogical-training-project-server.onrender.com/api',
-  withCredentials: true, // כדי שה-cookie של הסשן יעבוד
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
 });
+
 
 // גם ברירת מחדל וגם בשם — כדי שלא תצטרך/י לשנות ייבוא בקבצים אחרים
 export default axiosInstance;
